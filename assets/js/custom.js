63eeb2bd97d581922e9dccc87fa00ce71763256d
Ben Liu <yepkevin@gmail.com>
2015-02-09 22:17:29 +0000

    update
    small changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -37,7 +37,9 @@
 		$('#intro').backstretch([
 			'assets/images/bg3.jpg',
 			'assets/images/bg4.jpg',
-			'assets/images/bg2.jpg'
+			'assets/images/bg2.jpg',
+			'assets/images/bg5.jpg',
+			'assets/images/bg6.jpg'
 		], {duration: 3000, fade: 750});
 
 		/* ---------------------------------------------- /*
